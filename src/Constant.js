c974c3a8c7a5d82882c4d40469b5fbb68738c5c6
Arsalanbashir831 <arsalanbashir831@gmail.com>
2024-10-01 22:03:44 +0500

    update url of backend

--- src/Constant.js
+++ src/Constant.js
@@ -1,3 +1,7 @@
-export const BACKEND_URL='https://condominium-management-backend.onrender.com'
+export const BACKEND_URL='https://amministratori-condominiali-backend.onrender.com'
 // http://localhost:8080
 //https://condominium-management-backend.onrender.com
+
+//https://condominium-management-bot.vercel.app/
+
+//https://amministratori-condominiali-backend.onrender.com
